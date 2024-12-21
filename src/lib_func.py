658888167456230af050c106356f4cfed88327e5
jjboykin <jeremy.j.boykin@gmail.com>
2024-12-21 16:42:41 -0500

    markdown to html

--- src/lib_func.py
+++ src/lib_func.py
@@ -5,6 +5,14 @@ from htmlnode import *
 from leafnode import *
 from parentnode import *
 
+class HTMLBlock:
+    block_type_paragraph = "paragraph"
+    block_type_heading = "heading"
+    block_type_code = "code"
+    block_type_quote = "quote"
+    block_type_olist = "ordered_list"
+    block_type_ulist = "unordered_list"
+
 def text_node_to_html_node(text_node):
     match (text_node.text_type):
         case TextType.TEXT:
@@ -23,27 +31,24 @@ def text_node_to_html_node(text_node):
             raise Exception("Invalid text type")
 
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
-    nodes = []
-    for node in old_nodes:
-        # If an "old node" is not a TextType.TEXT type, just add it to the new list as-is, we only attempt to split "text" type objects (not bold, italic, etc).
-        if not node.text_type == TextType.TEXT:
-            nodes.append(node)
-        else:
-            # If a matching closing delimiter is not found, just raise an exception with a helpful error message, that's invalid Markdown syntax.
-            if not node.text.count(delimiter) % 2 == 0:
-                raise Exception("Invalid Markdown syntax")
-            
-            node_tokens = node.text.split(delimiter)
-            i = 0
-            for token in node_tokens:
-                if token:
-                    #print(token)
-                    if i % 2 == 0:
-                        nodes.append(TextNode(token,TextType.TEXT))
-                    else:
-                        nodes.append(TextNode(token,text_type))
-                    i += 1
-    return nodes
+    new_nodes = []
+    for old_node in old_nodes:
+        if old_node.text_type != TextType.TEXT:
+            new_nodes.append(old_node)
+            continue
+        split_nodes = []
+        sections = old_node.text.split(delimiter)
+        if len(sections) % 2 == 0:
+            raise ValueError("Invalid markdown, formatted section not closed")
+        for i in range(len(sections)):
+            if sections[i] == "":
+                continue
+            if i % 2 == 0:
+                split_nodes.append(TextNode(sections[i], TextType.TEXT))
+            else:
+                split_nodes.append(TextNode(sections[i], text_type))
+        new_nodes.extend(split_nodes)
+    return new_nodes
 
 def extract_markdown_images(text):
     # images
@@ -111,3 +116,152 @@ def text_to_textnodes(text):
     nodes = split_nodes_image(nodes)
     nodes = split_nodes_link(nodes)
     return nodes
+
+def markdown_to_blocks(markdown):
+    blocks = markdown.split("\n\n")
+    filtered_blocks = []
+    for block in blocks:
+        if block == "":
+            continue
+        block = block.strip()
+        filtered_blocks.append(block)
+    return filtered_blocks
+
+def block_to_block_type(block):
+    lines = block.split("\n")
+    
+    if block.startswith(("# ", "## ", "### ", "#### ", "##### ", "###### ")):
+        return HTMLBlock.block_type_heading
+    if len(lines) > 1 and lines[0].startswith("```") and lines[-1].startswith("```"):
+        return HTMLBlock.block_type_code
+    if block.startswith(">"):
+        for line in lines:
+            if not line.startswith(">"):
+                return HTMLBlock.block_type_paragraph
+        return HTMLBlock.block_type_quote
+    if block.startswith("* "):
+        for line in lines:
+            if not line.startswith("* "):
+                return HTMLBlock.block_type_paragraph
+        return HTMLBlock.block_type_ulist
+    if block.startswith("- "):
+        for line in lines:
+            if not line.startswith("- "):
+                return HTMLBlock.block_type_paragraph
+        return HTMLBlock.block_type_ulist
+    if block.startswith("1. "):
+        i = 1
+        for line in lines:
+            if not line.startswith(f"{i}. "):
+                return HTMLBlock.block_type_paragraph
+            i += 1
+        return HTMLBlock.block_type_olist
+    return HTMLBlock.block_type_paragraph
+
+def markdown_to_html_node(markdown):
+    children = []
+
+    # Split the markdown into blocks (you already have a function for this)
+    blocks = markdown_to_blocks(markdown)
+
+    # Loop over each block:
+    for block in blocks:
+        # Determine the type of block (you already have a function for this)
+        block_type = block_to_block_type(block)
+
+        # Based on the type of block, create a new HTMLNode with the proper data
+        match block_type:
+            case HTMLBlock.block_type_paragraph:
+                child_nodes = text_to_children(block)
+                has_children = len(child_nodes) > 1
+                if has_children:
+                    node = ParentNode("p", children=child_nodes)
+                else:
+                    node = LeafNode("p", value=block)
+            case HTMLBlock.block_type_heading:
+                if block.startswith("# "):
+                    heading_tag = "h1"
+                elif block.startswith("## "):
+                    heading_tag = "h2"
+                elif block.startswith("### "):
+                    heading_tag = "h3"
+                elif block.startswith("#### "):
+                    heading_tag = "h4"
+                elif block.startswith("##### "):
+                    heading_tag = "h5"
+                elif block.startswith("###### "):
+                    heading_tag = "h6"
+                block = strip_markdown(block, HTMLBlock.block_type_heading)
+                node = LeafNode(heading_tag, value=block)
+            case HTMLBlock.block_type_code:
+                block = strip_markdown(block, HTMLBlock.block_type_code)
+                node = LeafNode("code", value=block)
+                node = ParentNode("pre", children=node)
+            case HTMLBlock.block_type_quote:
+                block = strip_markdown(block, HTMLBlock.block_type_quote)
+                node = LeafNode("blockquote", value=block)
+            case HTMLBlock.block_type_olist:
+                block = strip_markdown(block, HTMLBlock.block_type_olist)
+                child_nodes = text_to_list_items(block)
+                node = ParentNode("ol", children=child_nodes)
+            case HTMLBlock.block_type_ulist:
+                block = strip_markdown(block, HTMLBlock.block_type_ulist)
+                child_nodes = text_to_list_items(block)
+                node = ParentNode("ul", children=child_nodes)
+            case _:
+                raise Exception("Invalid block type")
+
+        children.append(node)
+
+    # Make all the block nodes children under a single parent HTML node (which should just be a div) and return it.
+    return ParentNode("div", children)
+
+def text_to_children(text):
+    html_nodes = []
+
+    text_nodes = text_to_textnodes(text)
+    for text_node in text_nodes:
+        html_nodes.append(text_node_to_html_node(text_node))
+
+    return html_nodes
+
+def text_to_list_items(block):
+    html_nodes = []
+    lines = block.split("\n")
+
+    for line in lines:
+        list_item = LeafNode("li", value=line)
+        html_nodes.append(list_item)
+
+    return html_nodes
+
+def strip_markdown(block, block_type):
+    match block_type:
+        case HTMLBlock.block_type_heading:
+            if block.startswith("# "):
+                return block.replace("# ", "")
+            if block.startswith("## "):
+                return block.replace("## ", "")
+            if block.startswith("### "):
+                return block.replace("### ", "")
+            if block.startswith("#### "):
+                return block.replace("#### ", "")
+            if block.startswith("##### "):
+                return block.replace("##### ", "")
+            if block.startswith("###### "):
+                return block.replace("###### ", "")
+        case HTMLBlock.block_type_code:
+            return block.replace("```", "")
+        case HTMLBlock.block_type_quote:
+            return block.replace(">", "")
+        case HTMLBlock.block_type_ulist:
+            block = block.replace("* ", "")
+            return block.replace("- ", "")
+        case HTMLBlock.block_type_olist:
+            lines = block.split("\n")
+            i = 1
+            for line in lines:
+                line.replace(f"{i}. ", "")
+                i += 1
+            block = "\n".join(lines)
+            return block
--- src/main.py
+++ src/main.py
@@ -5,7 +5,20 @@ from parentnode import *
 from lib_func import *
 
 def main():
-    text = "This is **text** with an *italic* word and a `code block` and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://boot.dev)"
-    print(text_to_textnodes(text))
+    #text = "This is **text** with an *italic* word and a `code block` and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://boot.dev)"
+    #print(text_to_textnodes(text))
+
+    full_markdown = """# This is a heading
+
+This is a paragraph of text. It has some **bold** and *italic* words inside of it.
+
+* This is the first list item in a list block
+* This is a list item
+* This is another list item"""
+    print("Markdown:")
+    print(full_markdown)
+    print("HTML:")
+    print(markdown_to_html_node(full_markdown).to_html())
     
+
 main()
